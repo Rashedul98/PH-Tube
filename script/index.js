922e3c4a2c5c32b33fc58d9c302a62a1c1918a72
Rashedul98 <thisisrashedul@gmail.com>
2025-09-22 14:24:31 +0600

    on click on a certain button, Load specific Catagory Data of dynamic category section is now completed

--- script/index.js
+++ script/index.js
@@ -2,6 +2,7 @@
 const getAllCategoris = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     const data = await response.json();
+
     displayCategories(data.categories);
 
 }
@@ -15,7 +16,7 @@ const displayCategories = (data) => {
     for (const cats of data) {
         const DivOfButton = document.createElement('div');
         DivOfButton.innerHTML = `
-        <button class="btn btn-sm  hover:bg-[#FF1F3D] hover:text-white" >${cats.category} </button>
+        <button onclick=(getCategoryVideos(${cats.category_id})) class="btn btn-sm  hover:bg-[#FF1F3D] hover:text-white" >${cats.category} </button>
         `
         container.appendChild(DivOfButton);
     }
@@ -26,12 +27,12 @@ const displayCategories = (data) => {
 const getAllVideos = async () => {
     const getResponse = await fetch("https://openapi.programming-hero.com/api/phero-tube/videos");
     const datas = await getResponse.json();
-    console.log(datas.videos);
+    // console.log(datas.videos);
 
     checkVids(datas.videos);
 
 }
-getAllVideos();
+
 
 const checkVids = (videos) => {
     const container = document.getElementById('all-video-container');
@@ -59,7 +60,7 @@ const checkVids = (videos) => {
                         <p class="flex gap-2 text-gray-400 text-sm font-normal">${video.authors[0].profile_name}<img class="w-5"
                                 src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt="">
                         </p>
-                        <p class=" text-gray-400 text-sm font-normal">91k Views</p>
+                        <p class=" text-gray-400 text-sm font-normal">${video.others.views}</p>
                     </div>
                 </div>
             </div>
@@ -70,3 +71,50 @@ const checkVids = (videos) => {
     });
 
 }
+
+const getCategoryVideos = async (id) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`);
+    const data = await response.json();
+    viewCategoryVideo(data.category)
+}
+
+const viewCategoryVideo = (categories) => {
+    console.log(categories);
+
+    const container = document.getElementById("all-video-container");
+
+    for (const cats of categories) {
+        const vidContainer = document.createElement('div');
+        vidContainer.innerHTML = `
+                         <div class="card bg-base-100">
+                <figure class="relative">
+                    <img class="w-full h-[200px] object-cover"  src="${cats.thumbnail}" alt="Shoes" />
+                    <span class="absolute text-sm text-white bottom-2 right-2 px-2 rounded opacity-45 bg-black">3hrs
+                        56min ago </span>
+                </figure>
+                <div class="flex  gap-4 px-2 py-5">
+                    <div class="profile">
+                        <div class="avatar">
+                            <div class="ring-primary ring-offset-base-100 w-10 rounded-full ring-2 ring-offset-2">
+                                <img src="${cats.authors[0].profile_picture}" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="intro text-base font-bold">
+                        <h1>${cats.title}</h1>
+                        <p class="flex gap-2 text-gray-400 text-sm font-normal">${cats.authors[0].profile_name}<img class="w-5"
+                                src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt="">
+                        </p>
+                        <p class=" text-gray-400 text-sm font-normal">${cats.others.views}</p>
+                    </div>
+                </div>
+            </div>
+        `
+
+        container.appendChild(vidContainer);
+
+    }
+
+}
+
+
